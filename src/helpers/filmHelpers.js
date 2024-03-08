d8d90d2dd77eb6f3d80cb5b7834d1177ff9771da
Dmitry19001 <dmitry.gongalo@student.lab.fi>
2024-03-08 19:03:44 +0200

    helper function for film

---
+++ src/helpers/filmHelpers.js
@@ -0,0 +1,20 @@
+const User = require('../models/user');
+
+async function attachUsernames(films) {
+  const filmArray = Array.isArray(films) ? films : [films];
+  
+  const userIds = [...new Set(filmArray.map(film => film.addedBy.toString()))];
+  const users = await User.find({ '_id': { $in: userIds } });
+
+  const usernameMap = users.reduce((acc, user) => {
+    acc[user._id.toString()] = user.username;
+    return acc;
+  }, {});
+
+  return filmArray.map(film => ({
+    ...film.toObject(), // Convert Mongoose document to a plain object
+    addedBy: usernameMap[film.addedBy.toString()],
+  }));
+}
+
+module.exports = { attachUsernames };
