f8d03f729b17912628455051f144c088d5b759d4
Dmitry19001 <dmitry.gongalo@student.lab.fi>
2024-03-08 19:04:41 +0200

    improved filmsController to change user id with username

--- src/controllers/filmsController.js
+++ src/controllers/filmsController.js
@@ -1,38 +1,55 @@
 const Film = require('../models/film');
+const { attachUsernames } = require('../helpers/filmHelpers');
 
 exports.getFilms = async (req, res) => {
-  try {
-    const films = await Film.find();
-    res.json(films);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
+    try {
+      let films = await Film.find();
+      films = await attachUsernames(films);
+      res.json(films);
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
 };
 
 exports.createFilms = async (req, res) => {
-  try {
-    const film = new Film(req.body);
-    await film.save();
-    res.status(201).json(film);
-  } catch (err) {
-    res.status(400).send(err.message);
-  }
+    try {
+      if (!req.user || !req.user._id) {
+        return res.status(403).send("User not authenticated");
+      }
+  
+      const filmData = {
+        ...req.body,
+        addedBy: req.user._id,
+      };
+  
+      const film = new Film(filmData);
+      await film.save();
+  
+      const filmWithUsername = await attachUsernames(film);
+  
+      res.status(201).json(filmWithUsername[0]);
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
 };
 
 exports.updateFilm = async (req, res) => {
-  try {
-    const film = await Film.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.json(film);
-  } catch (err) {
-    res.status(400).send(err.message);
-  }
+    try {
+      let film = await Film.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      
+      const filmWithUsername = await attachUsernames(film);
+      res.json(filmWithUsername[0]);
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
 };
+  
 
 exports.deleteFilm = async (req, res) => {
-  try {
-    await Film.findByIdAndDelete(req.params.id);
-    res.status(204).send("Film deleted");
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
+    try {
+        await Film.findByIdAndDelete(req.params.id);
+        res.status(204).send("Film deleted");
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 };
