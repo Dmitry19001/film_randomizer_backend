41dcf978adce6afe078a6b876226299a43f1ff3e
Dmitry19001 <dmitry.gongalo@student.lab.fi>
2024-03-08 18:45:35 +0200

    film now has addedBy

--- src/models/film.js
+++ src/models/film.js
@@ -17,6 +17,11 @@ const filmSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Category',
   }],
+  addedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
   createdAt: {
     type: Date,
     default: Date.now,
