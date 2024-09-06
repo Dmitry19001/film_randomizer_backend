dc2a246055ab6ba2628e198f023325cb5bb3ff69
Dmitry Gongalo <dmitri.gongalo@gmail.com>
2024-09-06 21:41:04 +0300

    added login status check

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,12 @@
 const { login, register } = require('../controllers/authController');
 const express = require('express');
 const router = express.Router();
+const protect = require('../middleware/auth');
 
 router.post('/login', login);
 router.post('/register', register);
+router.get('/status', protect, (req, res) => {
+  res.status(200);
+})
 
 module.exports = router;
